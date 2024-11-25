9108afd2c02e4d5b5c3caa210e2aa43d202ef274
debora-os <debruxo31@gmail.com>
2024-11-25 00:26:07 -0300

    header

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import CreateAccountCuidador from "./pages/CreateAccountCuidador";
+import HomeCuidador from "./pages/HomeCuidador";
 
 const AppRouter = () => {
   return (
@@ -9,6 +10,7 @@ const AppRouter = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/create-account-cuidador" element={<CreateAccountCuidador />} />
+        <Route path="/home-cuidador" element={<HomeCuidador />} />
       </Routes>
     </Router>
   );
